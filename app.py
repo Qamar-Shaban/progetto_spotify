234456fad67aed315a3f9ea75e2f8e32fe0c55b2
Shaban Qamar <qamar.shaban@iisgalvanimi.edu.it>
2025-02-12 10:06:26 +0000

    spotify parte 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "b6c825a1f5364751a178db12c7373bd8"
 SPOTIFY_CLIENT_SECRET = "329a625d0f1e4a9c8a5b43918a91ae0d"
-SPOTIFY_REDIRECT_URI = "https://5000-qamarshaban-progettospo-1l98wyvo229.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-qamarshaban-progettospo-m4w1bysvb01.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -15,7 +15,8 @@ sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
-    scope="user-read-private" #permessi x informazioni dell'utente
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 @app.route('/')
@@ -30,6 +31,7 @@ def callback():
     session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
     return redirect(url_for('home'))
 
+
 @app.route('/home')
 def home():
     token_info = session.get('token_info', None) #recupero token sissione (salvato prima)
@@ -39,7 +41,14 @@ def home():
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
-
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
+    
+    return render_template('home.html', user_info=user_info, playlists=playlists_info)
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
+    
 if __name__ == '__main__':
     app.run(debug=True)
